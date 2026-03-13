cb037bac507f3494d77f71497f9ac06f21ecbf15
Antimo-M <montellaantimo@live.it>
2026-03-13 14:01:14 +0100

    feat: aggiunta la tabella con bootsrap e cambiato il modo in cui mostriamo in pagina  le chiavi e i valori dell'array usando sempre forEach, aggiunto anchee una condizione al parking, visto che non gradivo che uscisse 1 se true e nulla se false, adesso passiamo ai filtri.

--- index.php
+++ index.php
@@ -1,63 +1,85 @@
+<?php
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <title>Hotels</title>
 </head>
+
 <body>
-    <h1>Php Hotels</h1>
-
-    <?php
-
-    $hotels = [
-
-        [
-            'name' => 'Hotel Belvedere',
-            'description' => 'Hotel Belvedere Descrizione',
-            'parking' => true,
-            'vote' => 4,
-            'distance_to_center' => 10.4
-        ],
-        [
-            'name' => 'Hotel Futuro',
-            'description' => 'Hotel Futuro Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 2
-        ],
-        [
-            'name' => 'Hotel Rivamare',
-            'description' => 'Hotel Rivamare Descrizione',
-            'parking' => false,
-            'vote' => 1,
-            'distance_to_center' => 1
-        ],
-        [
-            'name' => 'Hotel Bellavista',
-            'description' => 'Hotel Bellavista Descrizione',
-            'parking' => false,
-            'vote' => 5,
-            'distance_to_center' => 5.5
-        ],
-        [
-            'name' => 'Hotel Milano',
-            'description' => 'Hotel Milano Descrizione',
-            'parking' => true,
-            'vote' => 2,
-            'distance_to_center' => 50
-        ],
-
-    ];
+    <h1 class="text-center py-2">Php Hotels</h1>
+    <div class="container mt-5">
+        <h2 class="mb-4 text-center">Hotel</h2>
+        <table class="table table-striped table-bordered">
+            <thead class="table-dark">
+                <tr>
+                    <th>name</th>
+                    <th>description</th>
+                    <th>parking</th>
+                    <th>vote</th>
+                    <th>distance_to_center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach($hotels as $hotel){
+                    echo "<tr>";
+                    echo "<td>" . $hotel["name"] . "</td>";
+                    echo "<td>" . $hotel["description"] . "</td>";
+                    echo "<td>" . ($hotel["parking"] ? "Disponibile" : "Non disponbiile") . "</td>";
+                    echo "<td>" . $hotel["vote"] . "</td>";
+                    echo "<td>" . $hotel["distance_to_center"] . "Km" . "</td>";
+                    echo "</tr>";
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
     
-     //iniziamo con lo stampare tutti i dati di questo array
-    foreach($hotels as $hotel){
-
-        foreach($hotel as $key => $value){
-            echo $key . " - " . $value . " / ";
-        }
-        echo "<br>";
-    }
-    ?>
 </body>
 </html>
